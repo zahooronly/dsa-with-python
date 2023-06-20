9219c59352087fd535c9c005672a9d3b09982c78
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-06-20 12:17:00 +0500

    append and printLL methods

--- linkedLists/llConstructor.py
+++ linkedLists/llConstructor.py
@@ -9,12 +9,38 @@ class LinkedList:
         self.head=new_value
         self.tail=new_value
         self.length=1
-
+    
+    
+    def printLL(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
+    
+    def append(self,value):
+        newNode=Node(value=value)
+        if self.head is None:
+            self.head=newNode
+            self.tail=newNode
+        else:
+            self.tail.next=newNode
+            self.tail=newNode
+        self.length+=1
+            
+            
 
 
  
 my_linked_list = LinkedList(4)
 
+
 print('Head:', my_linked_list.head.value)
 print('Tail:', my_linked_list.tail.value)
 print('Length:', my_linked_list.length)
+print(my_linked_list.printLL())
+my_linked_list.append(56)
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+print(my_linked_list.printLL())
+# my_linked_list.append(56)
---
+++ linkedLists/printLL.py
@@ -0,0 +1,5 @@
+def printLL(self):
+    temp=self.head
+    while temp is not None:
+        print(temp.value)
+        temp=temp.next
