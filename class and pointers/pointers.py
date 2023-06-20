af6e0662d76a9e8a6109b10881b95f819208d5d0
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-06-20 10:52:38 +0500

    pointers checked

--- class and pointers/pointers.py
+++ class and pointers/pointers.py
@@ -1,3 +1,15 @@
+a=10
+b=a
+print(f'a= {a}')
+print(f'b= {b}')
+print(f'a= {id(a)}')
+print(f'b= {id(b)}')
+b=12
+print(f'now a= {a}')
+print(f'now b= {b}')
+print(f'now a= {id(a)}')
+print(f'now b= {id(b)}')
+
 dict1={
     'value':11,
 }
@@ -7,5 +19,7 @@ print(f'location of dict2 is: {id(dict2)}')
 dict2['value']=45
 print(f'location of dict1 is: {id(dict1)}')
 print(f'location of dict2 is: {id(dict2)}')
+print(f'location of dict1 is: {dict1}')
+print(f'location of dict2 is: {dict2}')
 print(dict1)
 print(dict2)
